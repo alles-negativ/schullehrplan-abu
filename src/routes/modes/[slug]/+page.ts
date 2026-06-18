36f64861ea30bfc4a3902f1b34fa7ac8f059ddac
Aurelian Ammon <aurelianammon@gmail.com>
2026-06-18 20:01:45 +0200

    [feat]: add functionality to retrieve all education modes and map them to slugs for improved mode navigation

--- src/routes/modes/[slug]/+page.ts
+++ src/routes/modes/[slug]/+page.ts
@@ -1,7 +1,10 @@
 import { error } from '@sveltejs/kit';
-import { getEducationModeBySlug } from '$lib/data/education-modes';
+import { getAllEducationModes, getEducationModeBySlug } from '$lib/data/education-modes';
 
 import type { PageLoad } from './$types';
+
+export const entries = () =>
+	getAllEducationModes().map((mode) => ({ slug: mode.slug }));
 
 export const load: PageLoad = ({ params }) => {
 	const mode = getEducationModeBySlug(params.slug);
